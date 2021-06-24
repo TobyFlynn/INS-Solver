d2bc0e0f1731d120e9852fc0c0ff6d67c769ce44
Toby Flynn <toby_flynn@btinternet.com>
2021-06-24 17:56:30 +0100

    Poisson helper functions

--- include/pressure_solve.h
+++ include/pressure_solve.h
@@ -3,6 +3,8 @@
 
 #include "ins_data.h"
 #include "op_seq.h"
+#include "petscvec.h"
+#include "petscksp.h"
 
 class PressureSolve {
 public:
@@ -11,24 +13,40 @@ public:
 
   void init();
   bool solve(op_dat b_dat, op_dat x_dat);
+  void calc_rhs(const double *u_d, double *rhs_d);
 
   void setDirichletBCs(int *d);
   void setNeumannBCs(int *n);
   void setBCValues(op_dat bc);
   double getAverageConvergeIter();
 
+  op_dat u, rhs;
+
 private:
+  void create_vec(Vec *v, int size = 15);
+  void destroy_vec(Vec *v);
+  void load_vec(Vec *v, op_dat v_dat, int size = 15);
+  void store_vec(Vec *v, op_dat v_dat);
+  void copy_u(const double *u_d);
+  void copy_rhs(double *rhs_d);
+  void create_shell_mat(Mat *m);
+
   INSData *data;
   CubatureData *cData;
   GaussData *gData;
 
-  int *dirichlet;
-  int *neumann;
+  int *dirichlet, *neumann;
 
   int numberIter = 0;
   int solveCount = 0;
 
   op_dat bc_dat;
+
+  Mat Amat;
+  KSP ksp;
+  Vec b, x;
+
+  double *u_data, *rhs_data;
 };
 
 #endif
--- src/pressure_solve.cpp
+++ src/pressure_solve.cpp
@@ -6,10 +6,22 @@ PressureSolve::PressureSolve(INSData *d, CubatureData *c, GaussData *g) {
   data = d;
   cData = c;
   gData = g;
+
+  u_data   = (double *)calloc(15 * data->numCells, sizeof(double));
+  rhs_data = (double *)calloc(15 * data->numCells, sizeof(double));
+
+  u   = op_decl_dat(data->cells, 15, "double", u_data, "poisson_u");
+  rhs = op_decl_dat(data->cells, 15, "double", rhs_data, "poisson_rhs");
 }
 
 PressureSolve::~PressureSolve() {
+  free(u_data);
+  free(rhs_data);
 
+  destroy_vec(&b);
+  destroy_vec(&x);
+  KSPDestroy(&ksp);
+  MatDestroy(&Amat);
 }
 
 void PressureSolve::setDirichletBCs(int *d) {
@@ -32,9 +44,26 @@ double PressureSolve::getAverageConvergeIter() {
 }
 
 void PressureSolve::init() {
+  create_vec(&b);
+  create_vec(&x);
+  create_shell_mat(&Amat);
 
+  KSPCreate(PETSC_COMM_WORLD, &ksp);
+  KSPSetType(ksp, KSPCG);
+  KSPSetOperators(ksp, Amat, Amat);
+  KSPSetTolerances(ksp, 1e-8, 1e-50, 1e5, 1e5);
+  KSPSetInitialGuessNonzero(ksp, PETSC_TRUE);
 }
 
 bool PressureSolve::solve(op_dat b_dat, op_dat x_dat) {
   return false;
+}
+
+void PressureSolve::calc_rhs(const double *u_d, double *rhs_d) {
+  // Copy u to OP2 dat
+  copy_u(u_d);
+
+  
+
+  copy_rhs(rhs_d);
 }
