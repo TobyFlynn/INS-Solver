96eaa9dc261c7cca1cad3bb23ba28716d9dc731d
Toby Flynn <T.Flynn@warwick.ac.uk>
2022-08-14 16:10:15 +0100

    KD MPI Naive on GPU

--- src/utils.cu
+++ src/utils.cu
@@ -9,6 +9,7 @@ double *getOP2PtrDevice(op_dat dat, op_access acc) {
     op_arg_dat(dat, -1, OP_ID, dat->dim, "double", acc)
   };
   op_mpi_halo_exchanges_cuda(dat->set, 1, args);
+  op_mpi_wait_all(1, args);
   return (double *) dat->data_d;
 }
 
@@ -26,6 +27,7 @@ double *getOP2PtrHost(op_dat dat, op_access acc) {
     op_arg_dat(dat, -1, OP_ID, dat->dim, "double", acc)
   };
   op_mpi_halo_exchanges_cuda(dat->set, 1, args);
+  op_mpi_wait_all(1, args);
   double *res = (double *)malloc(dat->set->size * dat->dim * sizeof(double));
   cudaMemcpy(res, dat->data_d, dat->set->size * dat->dim * sizeof(double), cudaMemcpyDeviceToHost);
   return res;
@@ -42,6 +44,56 @@ void releaseOP2PtrHost(op_dat dat, op_access acc, const double *ptr) {
 
   op_mpi_set_dirtybit_cuda(1, args);
 
+  free((void *)ptr);
+  ptr = nullptr;
+}
+
+double *getOP2PtrDeviceMap(op_dat dat, op_map map, op_access acc) {
+  op_arg args[] = {
+    op_arg_dat(dat, 0, map, dat->dim, "double", acc),
+    op_arg_dat(dat, 1, map, dat->dim, "double", acc)
+  };
+  op_mpi_halo_exchanges_cuda(map->from, 2, args);
+  op_mpi_wait_all(2, args);
+  
+  return (double *) dat->data_d;
+}
+
+void releaseOP2PtrDeviceMap(op_dat dat, op_map map, op_access acc, const double *ptr) {
+  op_arg args[] = {
+    op_arg_dat(dat, 0, map, dat->dim, "double", acc),
+    op_arg_dat(dat, 1, map, dat->dim, "double", acc)
+  };
+  op_mpi_set_dirtybit_cuda(2, args);
+
+  ptr = nullptr;
+}
+
+double *getOP2PtrHostMap(op_dat dat, op_map map, op_access acc) {
+  op_arg args[] = {
+    op_arg_dat(dat, 0, map, dat->dim, "double", acc),
+    op_arg_dat(dat, 1, map, dat->dim, "double", acc)
+  };
+  op_mpi_halo_exchanges_cuda(map->from, 2, args);
+  op_mpi_wait_all(2, args);
+
+  // TODO check that this shouldn't be more than dat->set->size
+  double *res = (double *)malloc(dat->set->size * dat->dim * sizeof(double));
+  cudaMemcpy(res, dat->data_d, dat->set->size * dat->dim * sizeof(double), cudaMemcpyDeviceToHost);
+  return res;
+}
+
+void releaseOP2PtrHostMap(op_dat dat, op_map map, op_access acc, const double *ptr) {
+  op_arg args[] = {
+    op_arg_dat(dat, 0, map, dat->dim, "double", acc),
+    op_arg_dat(dat, 1, map, dat->dim, "double", acc)
+  };
+
+  if(acc != OP_READ) {
+    cudaMemcpy(dat->data_d, ptr, dat->set->size * dat->dim * sizeof(double), cudaMemcpyHostToDevice);
+  }
+
+  op_mpi_set_dirtybit(2, args);
   free((void *)ptr);
   ptr = nullptr;
 }
