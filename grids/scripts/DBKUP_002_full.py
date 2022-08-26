306590214c491e4e87ee3d8237cbcc7e7865ef11
Toby Flynn <T.Flynn@warwick.ac.uk>
2022-08-26 16:01:54 +0100

    Updated airfoil mesh to include outline for finer elements

--- grids/scripts/DBKUP_002_full.py
+++ grids/scripts/DBKUP_002_full.py
@@ -13,16 +13,11 @@ with open('DBKUP_002.csv') as csvfile:
   for row in csvreader:
     x.append(row[0])
     y.append(row[1])
-    # if i <= 48:
-    #   x_top.append(row[0])
-    #   y_top.append(row[1])
-    # else:
-    #   x_bottom.append(row[0])
-    #   y_bottom.append(row[1])
     i = i + 1
 
 with open('DBKUP_002_full.geo', 'w') as geofile:
-  line = "airfoil_tol = 25;\n"
+  # Actual airfoil
+  line = "airfoil_tol = 1;\n"
   geofile.write(line)
   line = "factor = 0.47 / 4.9e-4;\n"
   geofile.write(line)
@@ -31,6 +26,34 @@ with open('DBKUP_002_full.geo', 'w') as geofile:
     geofile.write(line)
   line = "Spline(0) = { 0 : %i, 0 };\nCurve Loop(1) = {0};\n" % (len(x) - 1)
   geofile.write(line)
+
+  # Larger outline for making mesh fine near airfoil plus outline of droplet path
+  line = "factor_outline = (0.47 / 4.9e-4) * 1.1;\n"
+  geofile.write(line)
+  line = "x_offset_outline = (factor_outline - factor) / 2.0;\n"
+  geofile.write(line)
+  for i in range(0, 143 - len(x)):
+    p_id = i + len(x)
+    line = "Point(%i) = { %8.8f * factor_outline - x_offset_outline, %8.8f * factor_outline, 0, airfoil_tol};\n" % (p_id,float(x[i]),float(y[i]))
+    geofile.write(line)
+  
+  p_id = 143
+  line = "droplet_release = -0.17 * factor;\n"
+  geofile.write(line)
+  line = "droplet_path_height = 10;\n"
+  geofile.write(line)
+  line = "Point(%i) = { droplet_release, -droplet_path_height, 0, airfoil_tol };\n" % (p_id)
+  geofile.write(line)
+  line = "Point(%i) = { droplet_release, droplet_path_height, 0, airfoil_tol };\n" % (p_id + 1)
+  geofile.write(line)
+  
+  for i in range(145 - len(x) + 1, len(x)):
+    p_id = i + len(x) - 1
+    line = "Point(%i) = { %8.8f * factor_outline - x_offset_outline, %8.8f * factor_outline, 0, airfoil_tol};\n" % (p_id,float(x[i]),float(y[i]))
+    geofile.write(line)
+  line = "Spline(1) = { %i : %i, %i };\n" % (len(x), 2 * len(x) - 2, len(x))
+  geofile.write(line)
+
   boundaryPts = \
 """boundary_tol = 100;
 xL = -2 * factor;
@@ -50,7 +73,8 @@ l3 = newl; Line(l3) = {p3, p4};
 l4 = newl; Line(l4) = {p4, p1};
 
 Curve Loop(2) = {l1, l2, l3, l4};
-Plane Surface(1) = {2, 1};"""
+Plane Surface(1) = {2, 1};
+Curve{1} In Surface{1};\n"""
   geofile.write(boundaryPts)
 
 
