fd90910ca87d0929e86bfe8261d6c751c629e12c
Toby Flynn <toby_flynn@btinternet.com>
2023-02-10 12:44:02 +0000

    3D Advection test

--- src/kernels/advec_test_3d.h
+++ src/kernels/advec_test_3d.h
@@ -4,6 +4,6 @@ inline void advec_test_3d(const double *x, const double *y, const double *z,
     u[i] = 1.0;
     v[i] = 1.0;
     w[i] = 1.0;
-    val[i] = fmax(-(sqrt(x[i] * x[i] + y[i] * y[i]) - 3.0), 0.0);
+    val[i] = fmax(-(sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]) - 3.0), 0.0);
   }
 }
