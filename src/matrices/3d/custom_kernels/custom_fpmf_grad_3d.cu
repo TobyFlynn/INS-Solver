e121beb91d219fc12a070b8930fa5c04d3567ae6
Toby Flynn <toby_flynn@btinternet.com>
2023-05-17 21:14:44 +0100

    Experimenting

--- src/matrices/3d/custom_kernels/custom_fpmf_grad_3d.cu
+++ src/matrices/3d/custom_kernels/custom_fpmf_grad_3d.cu
@@ -5,30 +5,38 @@
 #include "dg_compiler_defs.h"
 #include "dg_global_constants/dg_mat_constants_3d.h"
 
-template<int dg_np>
+template<int dg_np, int NS>
 __device__ void _fpmf_grad_3d_gpu(const int ind, const DG_FP *dr, const DG_FP *ds,
                              const DG_FP *dt, const DG_FP *u, const DG_FP *fact,
                              const DG_FP *geof, DG_FP *ux, DG_FP *uy, DG_FP *uz) {
   if(!(ind < dg_np)) return;
 
-  DG_FP tmp_r = 0.0;
-  DG_FP tmp_s = 0.0;
-  DG_FP tmp_t = 0.0;
+  DG_FP tmp_r[NS] = {};
+  DG_FP tmp_s[NS] = {};
+  DG_FP tmp_t[NS] = {};
+  #pragma unroll
   for(int n = 0; n < dg_np; n++) {
-    int mat_ind = DG_MAT_IND(ind, n, dg_np, dg_np);
-    tmp_r += dr[mat_ind] * u[n];
-    tmp_s += ds[mat_ind] * u[n];
-    tmp_t += dt[mat_ind] * u[n];
+    const DG_FP u_n = u[n];
+    #pragma unroll
+    for(int i = 0; i < NS; i++) {
+      int mat_ind = DG_MAT_IND(ind + i, n, dg_np, dg_np);
+      tmp_r[i] += dr[mat_ind] * u_n;
+      tmp_s[i] += ds[mat_ind] * u_n;
+      tmp_t[i] += dt[mat_ind] * u_n;
+    }
   }
 
-  const DG_FP fact_ = fact[ind];
-  ux[ind] = fact_ * (geof[RX_IND] * tmp_r + geof[SX_IND] * tmp_s + geof[TX_IND] * tmp_t);
-  uy[ind] = fact_ * (geof[RY_IND] * tmp_r + geof[SY_IND] * tmp_s + geof[TY_IND] * tmp_t);
-  uz[ind] = fact_ * (geof[RZ_IND] * tmp_r + geof[SZ_IND] * tmp_s + geof[TZ_IND] * tmp_t);
+  #pragma unroll
+  for(int i = 0; i < NS; i++) {
+    const DG_FP fact_ = fact[ind + i];
+    ux[ind + i] = fact_ * (geof[RX_IND] * tmp_r[i] + geof[SX_IND] * tmp_s[i] + geof[TX_IND] * tmp_t[i]);
+    uy[ind + i] = fact_ * (geof[RY_IND] * tmp_r[i] + geof[SY_IND] * tmp_s[i] + geof[TY_IND] * tmp_t[i]);
+    uz[ind + i] = fact_ * (geof[RZ_IND] * tmp_r[i] + geof[SZ_IND] * tmp_s[i] + geof[TZ_IND] * tmp_t[i]);
+  }
 }
 
 // CUDA kernel function
-template<int p, int NUM_CELLS>
+template<int p, int NUM_CELLS, int NS>
 __global__ void _op_cuda_fpmf_grad_3d(
   const int *__restrict arg0,
   const DG_FP *__restrict arg4,
@@ -43,7 +51,7 @@ __global__ void _op_cuda_fpmf_grad_3d(
   __shared__ DG_FP dr_shared[DG_NP * DG_NP];
   __shared__ DG_FP ds_shared[DG_NP * DG_NP];
   __shared__ DG_FP dt_shared[DG_NP * DG_NP];
-  __shared__ DG_FP u_shared[NUM_CELLS * DG_NP];
+  __shared__ DG_FP u_shared[NS * NUM_CELLS * DG_NP];
 
   const int start_ind_mat = (p - 1) * DG_NP * DG_NP;
   for(int i = threadIdx.x; i < DG_NP * DG_NP; i += blockDim.x) {
@@ -54,22 +62,21 @@ __global__ void _op_cuda_fpmf_grad_3d(
 
   __syncthreads();
 
-  int n = threadIdx.x + blockIdx.x * blockDim.x;
+  const int n = threadIdx.x * NS + blockIdx.x * blockDim.x * NS;
   if(n < set_size * DG_NP) {
     const int node_id = n % DG_NP;
     const int cell_id = n / DG_NP;
-    const int local_cell_id = (n / DG_NP) - ((n - threadIdx.x) / DG_NP);
+    const int local_cell_id = (n / DG_NP) - ((n - NS * threadIdx.x) / DG_NP);
     // If entire thread is in set
-    if(n - threadIdx.x + blockDim.x < set_size * DG_NP) {
-      const int start_ind = ((n - threadIdx.x) / DG_NP) * DG_NP;
-      const int num_elem  = ((n - threadIdx.x + blockDim.x) / DG_NP) - ((n - threadIdx.x) / DG_NP) + 1;
+    if(n - NS * threadIdx.x + NS * blockDim.x < set_size * DG_NP) {
+      const int start_ind = ((n - NS * threadIdx.x) / DG_NP) * DG_NP;
+      const int num_elem  = ((n - NS * threadIdx.x + NS * blockDim.x) / DG_NP) - ((n - NS * threadIdx.x) / DG_NP) + 1;
       for(int i = threadIdx.x; i < num_elem * DG_NP; i += blockDim.x) {
         u_shared[i] = arg4[start_ind + i];
       }
-      // u_shared[threadIdx.x] = arg4[cell_id * DG_NP + node_id];
       __syncthreads();
 
-      _fpmf_grad_3d_gpu<np>(node_id,
+      _fpmf_grad_3d_gpu<np,NS>(node_id,
                 dr_shared,
                 ds_shared,
                 dt_shared,
@@ -80,7 +87,7 @@ __global__ void _op_cuda_fpmf_grad_3d(
                 arg15 + cell_id * DG_NP,
                 arg16 + cell_id * DG_NP);
     } else {
-      _fpmf_grad_3d_gpu<np>(node_id,
+      _fpmf_grad_3d_gpu<np,NS>(node_id,
                 dr_shared,
                 ds_shared,
                 dt_shared,
@@ -128,18 +135,18 @@ void custom_kernel_fpmf_grad_3d(const int order, char const *name, op_set set,
     //set CUDA execution parameters
     const int nthread = 256;
     // const int nblocks = 200 < (set->size * DG_NP) / nthread + 1 ? 200 : (set->size * DG_NP) / nthread + 1;
-    const int nblocks = (set->size * DG_NP) / nthread + 1;
+    const int nblocks = order == 3 ? (set->size * DG_NP) / (nthread * 2) + 1 : (set->size * DG_NP) / nthread + 1;
     const int num_cells = (nthread / DG_NP) + 2;
 
-    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<1,num_cells>, cudaFuncCachePreferShared));
-    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<2,num_cells>, cudaFuncCachePreferShared));
-    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<3,num_cells>, cudaFuncCachePreferShared));
-    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<4,num_cells>, cudaFuncCachePreferShared));
-    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<5,num_cells>, cudaFuncCachePreferShared));
+    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<1,num_cells,1>, cudaFuncCachePreferShared));
+    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<2,num_cells,1>, cudaFuncCachePreferShared));
+    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<3,num_cells,2>, cudaFuncCachePreferShared));
+    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<4,num_cells,1>, cudaFuncCachePreferShared));
+    cutilSafeCall(cudaFuncSetCacheConfig(_op_cuda_fpmf_grad_3d<5,num_cells,1>, cudaFuncCachePreferShared));
 
     switch(order) {
       case 1:
-        _op_cuda_fpmf_grad_3d<1,num_cells><<<nblocks,nthread>>>(
+        _op_cuda_fpmf_grad_3d<1,num_cells,1><<<nblocks,nthread>>>(
           (int *) arg0.data_d,
           (DG_FP *) arg4.data_d,
           (DG_FP *) argFactor.data_d,
@@ -150,7 +157,7 @@ void custom_kernel_fpmf_grad_3d(const int order, char const *name, op_set set,
           set->size );
         break;
       case 2:
-        _op_cuda_fpmf_grad_3d<2,num_cells><<<nblocks,nthread>>>(
+        _op_cuda_fpmf_grad_3d<2,num_cells,1><<<nblocks,nthread>>>(
           (int *) arg0.data_d,
           (DG_FP *) arg4.data_d,
           (DG_FP *) argFactor.data_d,
@@ -162,7 +169,7 @@ void custom_kernel_fpmf_grad_3d(const int order, char const *name, op_set set,
         break;
       case 3:
         timer->startTimer("fpmf_grad 3rd order");
-        _op_cuda_fpmf_grad_3d<3,num_cells><<<nblocks,nthread>>>(
+        _op_cuda_fpmf_grad_3d<3,num_cells,2><<<nblocks,nthread>>>(
           (int *) arg0.data_d,
           (DG_FP *) arg4.data_d,
           (DG_FP *) argFactor.data_d,
@@ -175,7 +182,7 @@ void custom_kernel_fpmf_grad_3d(const int order, char const *name, op_set set,
         timer->endTimer("fpmf_grad 3rd order");
         break;
       case 4:
-        _op_cuda_fpmf_grad_3d<4,num_cells><<<nblocks,nthread>>>(
+        _op_cuda_fpmf_grad_3d<4,num_cells,1><<<nblocks,nthread>>>(
           (int *) arg0.data_d,
           (DG_FP *) arg4.data_d,
           (DG_FP *) argFactor.data_d,
@@ -186,7 +193,7 @@ void custom_kernel_fpmf_grad_3d(const int order, char const *name, op_set set,
           set->size );
         break;
       case 5:
-        _op_cuda_fpmf_grad_3d<5,num_cells><<<nblocks,nthread>>>(
+        _op_cuda_fpmf_grad_3d<5,num_cells,1><<<nblocks,nthread>>>(
           (int *) arg0.data_d,
           (DG_FP *) arg4.data_d,
           (DG_FP *) argFactor.data_d,
