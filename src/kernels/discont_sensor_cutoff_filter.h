3ff38cfd5438a9a048a633b03ae89199d275aa24
Toby Flynn <toby_flynn@btinternet.com>
2023-07-10 16:33:22 +0100

    Wrong log

--- src/kernels/discont_sensor_cutoff_filter.h
+++ src/kernels/discont_sensor_cutoff_filter.h
@@ -28,7 +28,7 @@ inline void discont_sensor_cutoff_filter(const DG_FP *_max_alpha, const DG_FP *s
   }
 
   // DG_FP se = log10(u_hat_ip / u_ip);
-  DG_FP se = log10(fmin(*c * DG_ORDER * DG_ORDER * DG_ORDER * DG_ORDER * (u_hat_ip / u_ip), 1.0));
+  DG_FP se = log(fmin(*c * DG_ORDER * DG_ORDER * DG_ORDER * DG_ORDER * (u_hat_ip / u_ip), 1.0));
   const DG_FP min_alpha = 0.0;
   const DG_FP max_alpha = *_max_alpha;
   const int cutoff_order = DG_ORDER;
