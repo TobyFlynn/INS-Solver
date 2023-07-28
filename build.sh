9cac39140e354f2f8d332d371b6836ed3d8a335a
Toby Flynn <toby_flynn@btinternet.com>
2023-07-28 20:43:35 +0100

    Update AMGX json

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ mkdir -p linear_solvers/initial_guess_extrapolation
 cd ..
 
 ORDER=3
-SOA=0
+SOA=1
 
 python3 preprocessor.py 2 $ORDER
 
@@ -125,7 +125,7 @@ cd build
 #-DHYPRE_DIR=/home/u1717021/Code/PhD/hypre-install \
 
 cmake .. \
-  -DOP2_DIR=/home/u1717021/Code/PhD/OP2-Common/op2 \
+  -DOP2_DIR=/home/u1717021/Code/PhD/OP2-My-Fork/op2 \
   -DOPENBLAS_DIR=/opt/OpenBLAS \
   -DPETSC_DIR=/home/u1717021/Code/PhD/petsc/arch-linux-c-debug \
   -DPART_LIB_NAME=PARMETIS \
@@ -134,10 +134,11 @@ cmake .. \
   -DHDF5_DIR=/usr/local/module-software/hdf5-1.12.0-parallel \
   -DARMA_DIR=/home/u1717021/Code/PhD/armadillo-10.5.3/build \
   -DINIPP_DIR=/home/u1717021/Code/PhD/inipp/inipp \
+  -DHYPRE_DIR=/home/u1717021/Code/PhD/hypre-cpu-sp-install \
   -DORDER=$ORDER \
   -DSOA=$SOA \
   -DBUILD_SN=ON \
-  -DBUILD_CPU=ON \
+  -DBUILD_CPU=OFF \
   -DBUILD_MPI=ON \
   -DBUILD_GPU=ON
 
