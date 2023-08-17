dc1157b575821bb3d85aecf21120526bf0f1e65f
TobyFlynn <toby_flynn@btinternet.com>
2023-08-17 06:18:37 +0100

    Cirrus files

---
+++ build-cirrus.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build
+rm -rf gen2d
+rm -rf gen3d
+
+mkdir gen2d
+cd gen2d
+
+mkdir -p kernels
+mkdir -p solvers/2d/ls_utils
+mkdir -p solvers/3d/ls_utils
+mkdir -p matrices/2d
+mkdir -p matrices/3d/custom_kernels
+mkdir -p linear_solvers/amgx_amg
+mkdir -p linear_solvers/hypre_amg
+mkdir -p linear_solvers/petsc_amg
+mkdir -p linear_solvers/petsc_utils
+mkdir -p linear_solvers/petsc_block_jacobi
+mkdir -p linear_solvers/petsc_jacobi/custom_kernels
+mkdir -p linear_solvers/pmultigrid/custom_kernels
+mkdir -p linear_solvers/petsc_pmultigrid
+mkdir -p linear_solvers/petsc_inv_mass
+mkdir -p linear_solvers/initial_guess_extrapolation
+
+cd ..
+
+mkdir gen3d
+cd gen3d
+
+mkdir -p kernels
+mkdir -p solvers/2d/ls_utils
+mkdir -p solvers/3d/ls_utils
+mkdir -p matrices/2d
+mkdir -p matrices/3d/custom_kernels
+mkdir -p linear_solvers/amgx_amg
+mkdir -p linear_solvers/petsc_amg
+mkdir -p linear_solvers/hypre_amg
+mkdir -p linear_solvers/petsc_utils
+mkdir -p linear_solvers/petsc_block_jacobi
+mkdir -p linear_solvers/petsc_jacobi/custom_kernels
+mkdir -p linear_solvers/pmultigrid/custom_kernels
+mkdir -p linear_solvers/petsc_pmultigrid
+mkdir -p linear_solvers/petsc_inv_mass
+mkdir -p linear_solvers/initial_guess_extrapolation
+
+cd ..
+
+ORDER=3
+SOA=1
+
+python3 preprocessor.py 2 $ORDER
+
+python3 preprocessor.py 3 $ORDER
+
+if [ $SOA = 1 ]; then
+  export OP_AUTO_SOA=1
+fi
+
+cd gen2d
+
+# python3 $OP2_TRANSLATOR ins.cpp \
+#         ins_data.cpp solver.cpp poisson/petsc/poisson.cpp \
+#         ls/ls.cpp utils.cpp utils.cu ls/ls_reinit.cpp ls/ls_reinit.cu \
+#         ls/ls_reinit_mpi.cpp ls/ls_reinit_mpi.cu ls/ls_reinit_mpi_naive.cpp \
+#         ls/ls_reinit_mpi_naive.cu poisson/matrix/poisson_mat.cpp \
+#         poisson/p_multigrid/p_multigrid.cpp kernels/
+
+# sed -i '10i extern double reynolds;' openmp/ins_kernels.cpp
+
+python3 $OP2_TRANSLATOR ins2d.cpp \
+        solvers/2d/advection_solver.cpp \
+        solvers/2d/advection_solver_over_int.cpp \
+        solvers/2d/ls_solver.cpp \
+        solvers/2d/mp_ins_solver_over_int.cpp \
+        solvers/2d/ins_solver_base.cpp \
+        solvers/2d/ins_solver.cpp \
+        solvers/2d/mp_ins_solver.cpp \
+        solvers/2d/ins_solver_over_int.cpp \
+        solvers/2d/ce_solver_over_int.cpp \
+        kernels/
+
+sed -i "4i #include \"dg_compiler_defs.h\"" cuda/ins2d_kernels.cu
+sed -i "4i #include \"dg_compiler_defs.h\"" openmp/ins2d_kernels.cpp
+sed -i "4i #include \"dg_compiler_defs.h\"" seq/ins2d_seqkernels.cpp
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_2d.h\"" cuda/ins2d_kernels.cu
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_2d.h\"" openmp/ins2d_kernels.cpp
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_2d.h\"" seq/ins2d_seqkernels.cpp
+sed -i "5i #include \"cblas.h\"" openmp/ins2d_kernels.cpp
+sed -i "5i #include \"cblas.h\"" seq/ins2d_seqkernels.cpp
+
+cd ..
+
+cd gen3d
+
+python3 $OP2_TRANSLATOR ins3d.cpp \
+        solvers/3d/advection_solver.cpp \
+        solvers/3d/ins_solver_base.cpp \
+        solvers/3d/ins_solver.cpp \
+        solvers/3d/ls_solver.cpp \
+        solvers/3d/mp_ins_solver.cpp \
+        kernels/
+
+sed -i "4i #include \"dg_compiler_defs.h\"" cuda/ins3d_kernels.cu
+sed -i "4i #include \"dg_compiler_defs.h\"" openmp/ins3d_kernels.cpp
+sed -i "4i #include \"dg_compiler_defs.h\"" seq/ins3d_seqkernels.cpp
+sed -i "5i #include \"liquid_whistle_consts.h\"" cuda/ins3d_kernels.cu
+sed -i "5i #include \"liquid_whistle_consts.h\"" openmp/ins3d_kernels.cpp
+sed -i "5i #include \"liquid_whistle_consts.h\"" seq/ins3d_seqkernels.cpp
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_3d.h\"" cuda/ins3d_kernels.cu
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_3d.h\"" openmp/ins3d_kernels.cpp
+sed -i "6i #include \"dg_global_constants/dg_mat_constants_3d.h\"" seq/ins3d_seqkernels.cpp
+sed -i "7i #include \"cblas.h\"" openmp/ins3d_kernels.cpp
+sed -i "7i #include \"cblas.h\"" seq/ins3d_seqkernels.cpp
+
+cd ..
+
+mkdir build
+
+cd build
+
+#-DAMGX_DIR=/home/u1717021/Code/PhD/AMGX-install \
+#-DHYPRE_DIR=/home/u1717021/Code/PhD/hypre-install \
+
+cmake .. \
+  -DCMAKE_C_COMPILER=mpicc \
+  -DCMAKE_CXX_COMPILER=mpicxx \
+  -DOP2_DIR=$OP2_DIR \
+  -DOPENBLAS_DIR=$OPENBLAS_DIR \
+  -DPETSC_DIR=$PETSC_DIR \
+  -DHYPRE_DIR=/work/e01/e01/tflynne01/code/hypre-sp-install \
+  -DPART_LIB_NAME=PTSCOTCH \
+  -DPTSCOTCH_DIR=$PTSCOTCH_DIR \
+  -DOP2DGTOOLKIT_DIR=$OP2DGTOOLKIT_DIR \
+  -DHDF5_DIR=$HDF5_DIR \
+  -DARMA_DIR=$ARMA_DIR \
+  -DAMGX_DIR=/work/e01/e01/tflynne01/code/AMGX-install \
+  -DINIPP_DIR=$INIPP_DIR \
+  -DORDER=$ORDER \
+  -DSOA=$SOA \
+  -DBUILD_SN=OFF \
+  -DBUILD_CPU=OFF \
+  -DBUILD_MPI=ON \
+  -DBUILD_GPU=ON
+
+make -j 4
+make
