66aad4cf785e4b2230aeb26a6531f161666274ec
Toby Flynn <toby_flynn@btinternet.com>
2023-11-03 15:21:57 +0000

    Fixed incorrect sign with gravitational force

--- src/kernels/ins_2d_st_6_grav.h
+++ src/kernels/ins_2d_st_6_grav.h
@@ -3,6 +3,6 @@ inline void ins_2d_st_6_grav(const DG_FP *curv, DG_FP *st_x, DG_FP *st_y) {
     st_x[i] *= curv[i] / weber;
     st_y[i] *= curv[i] / weber;
     // Apply gravity
-    st_y[i] -= 1.0 / (froude * froude);
+    st_y[i] += 1.0 / (froude * froude);
   }
 }
