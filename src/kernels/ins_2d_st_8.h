e1f208a9a6d8c7aadaf6e8e8aa3b5fc708e91fc0
Toby Flynn <toby_flynn@btinternet.com>
2023-11-21 14:19:24 +0000

    Curvature correction minor change

--- src/kernels/ins_2d_st_8.h
+++ src/kernels/ins_2d_st_8.h
@@ -1,7 +1,6 @@
 inline void ins_2d_st_8(const DG_FP *alpha, const DG_FP *s, DG_FP *curv) {
   const DG_FP _alpha = *alpha;
   for(int i = 0; i < DG_NP; i++) {
-    if(fabs(s[i]) < _alpha)
-      curv[i] = 1.0 / ((1.0 / curv[i]) - s[i]);
+    curv[i] = 1.0 / ((1.0 / curv[i]) - s[i]);
   }
 }
