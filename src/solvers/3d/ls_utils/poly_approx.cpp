e4441155fe1852c87940d5887dc05c5376138878
Toby Flynn <toby_flynn@btinternet.com>
2024-01-10 14:08:58 +0000

    Minor edit in how polynomial for LS is constructed

--- src/solvers/3d/ls_utils/poly_approx.cpp
+++ src/solvers/3d/ls_utils/poly_approx.cpp
@@ -119,6 +119,38 @@ void PolyApprox3D::stencil_data(const int cell_ind, const set<int> &stencil,
                               vector<DG_FP> &z, vector<DG_FP> &s) {
   map<Coord, Point, cmpCoords> pointMap;
 
+  for(int n = 0; n < DG_NP; n++) {
+    int ind = cell_ind * DG_NP + n;
+    Coord coord;
+    coord.x = x_ptr[ind] - offset_x;
+    coord.y = y_ptr[ind] - offset_y;
+    coord.z = z_ptr[ind] - offset_z;
+    Point point;
+    auto res = pointMap.insert(make_pair(coord, point));
+    res.first->second.coord = coord;
+    res.first->second.val   = s_ptr[ind];
+    res.first->second.count = 1;
+  }
+
+  for(const auto &sten : stencil) {
+    if(sten == cell_ind) continue;
+    for(int n = 0; n < DG_NP; n++) {
+      int ind = sten * DG_NP + n;
+
+      Coord coord;
+      coord.x = x_ptr[ind] - offset_x;
+      coord.y = y_ptr[ind] - offset_y;
+      coord.z = z_ptr[ind] - offset_z;
+      Point point;
+      auto res = pointMap.find(coord);
+      if(res == pointMap.end()) continue;
+
+      res->second.val += s_ptr[ind];
+      res->second.count++;
+    }
+  }
+
+/*
   for(const auto &sten : stencil) {
     for(int n = 0; n < DG_NP; n++) {
       int ind = sten * DG_NP + n;
@@ -145,7 +177,7 @@ void PolyApprox3D::stencil_data(const int cell_ind, const set<int> &stencil,
       }
     }
   }
-
+*/
   for(auto const &p : pointMap) {
     x.push_back(p.second.coord.x);
     y.push_back(p.second.coord.y);
