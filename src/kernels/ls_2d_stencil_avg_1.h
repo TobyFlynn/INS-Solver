20dc41da74040848e7c78dc950cdeafadec03528
Toby Flynn <toby_flynn@btinternet.com>
2024-09-02 16:17:25 +0100

    bug fix

--- src/kernels/ls_2d_stencil_avg_1.h
+++ src/kernels/ls_2d_stencil_avg_1.h
@@ -6,7 +6,7 @@ inline void ls_2d_stencil_avg_1(const DG_FP *stencil, const DG_FP *avg, DG_FP *s
   for(int i = 0; i < DG_NUM_FACES * DG_NPF; i++) {
     if(avg[i] != 0.0) {
       int ind = fmask[i];
-      s[i] = avg[i];
+      s[ind] = avg[i];
     }
   }
 }
